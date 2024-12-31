e57d9b17e26296c763ed4b70660a144ebb57e155
erfan ashki <erfanbayern111@gmail.com>
2024-12-31 13:47:38 +0330

    add source to fields & some changes in ProductSerializer

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
-
+from django.utils import timezone
 
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     name = serializers.CharField(max_length=150)
-    unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
+    body = serializers.CharField(max_length=1000, source='description')
+    price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
+    slug = serializers.SlugField()
+    inventory = serializers.IntegerField()
+    datetime_created = serializers.DateTimeField(default=timezone.now)
+
 
     
