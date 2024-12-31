9814d4ed69687c924431556702dee2c6f6f3d54b
erfan ashki <erfanbayern111@gmail.com>
2024-12-31 17:37:55 +0330

    implement CategorySerializer and ProductSerializer by ModelSerializer

--- shop/serializers.py
+++ shop/serializers.py
@@ -8,24 +8,26 @@ from .models import Product, Category
 DOLLAR_TO_RIAL = 800000
 
 
-class CategorySerializer(serializers.Serializer):
-    id = serializers.IntegerField()
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'description']
+        
     name = serializers.CharField(max_length=150, source='title')
-    description = serializers.CharField(max_length=1000)
 
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    name = serializers.CharField(max_length=150)
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id','name','body','category','price','slug','inventory','datetime_created','rial_price','price_with_tax']
+    
     body = serializers.CharField(max_length=1000, source='description')
     category = serializers.HyperlinkedRelatedField(
         queryset=Category.objects.all() ,
         view_name='category_detail'
     ) 
     price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
-    slug = serializers.SlugField()
-    inventory = serializers.IntegerField()
-    datetime_created = serializers.DateTimeField(default=timezone.now)
     rial_price = serializers.SerializerMethodField()
     price_with_tax = serializers.SerializerMethodField(method_name='calc_tat')
     
