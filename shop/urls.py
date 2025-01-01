c69ef76f60c84ec5933b1283ced34c1b48a1d09e
erfan ashki <erfanbayern111@gmail.com>
2025-01-01 21:40:38 +0330

    implement routers for ProductViewSet & CategoryViewSet

--- shop/urls.py
+++ shop/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
+from rest_framework.routers import SimpleRouter, DefaultRouter
 
 from . import views
 
-urlpatterns = [
-    path('products/', views.ProductList.as_view(), name='product_list'),
-    path('products/<int:pk>/', views.ProductDetail.as_view(), name='product_detail'),
-    path('category/', views.CategoryList.as_view(), name='category_list'),
-    path('category/<int:pk>/', views.CategoryDetail.as_view(), name='category_detail'),
 
-]
+router = SimpleRouter()
+
+router.register('products', views.ProductViewSet, basename='product')
+router.register('category', views.CategoryViewSet, basename='category')
+
+urlpatterns = router.urls 
+
+
