e09050d974b19b2159f234829408d78aed798c20
erfan ashki <erfanbayern111@gmail.com>
2025-01-02 19:13:24 +0330

    use custom filter by the side of filterset_fields

--- shop/filters.py
+++ shop/filters.py
@@ -1,12 +1,16 @@
-from django_filters.rest_framework import FilterSet
-
 from .models import Product
 
+from django_filters import rest_framework as filters
 
-class ProductFilter(FilterSet):
+class ProductFilter(filters.FilterSet):
     class Meta:
         model = Product
-        fields = {
-            'inventory': ['lt', 'exact', 'gt']
-        }
+        fields = ['category']
         
+    inventory_range = filters.RangeFilter(field_name="inventory", label='enter inventory_range')
+    
+    # category_title = filters.CharFilter(method="filter_by_category", label='category title ')
+    
+    # def filter_by_category(self, queryset, name, value):
+    #     return queryset.filter(category__title=value)
+
