687139634230dc742c67d9fa88867de76bdda3ac
erfan ashki <erfanbayern111@gmail.com>
2025-01-05 22:22:32 +0330

    add signals in project between CustomUser & Customer

---
+++ shop/signals.py
@@ -0,0 +1,11 @@
+from django.db.models.signals import post_save,post_delete,post_migrate,post_init
+from django.dispatch import receiver
+from django.conf import settings
+
+from .models import Customer
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_customer_for_new_user(sender, instance, created, **kwargs):
+    if created:
+        Customer.objects.create(user=instance)
